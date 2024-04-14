aa3fa32a5b3cedf0f2e748ebff87d7d433cfd2d5
Claudio <ilclaudio@gmail.com>
2024-04-15 00:15:38 +0200

    New feature: Load example data: books, sections, authors and publishers

---
+++ example-data.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * Default data of the plugin.
+ *
+ * @package @package WP_KK_Writer_Plugin
+ */
+
+define(
+	'KKW_BLOG_TYPE_TERMS',
+	array(
+		'Article',
+		'Event',
+		'News',
+	)
+);
+
+define(
+	'KKW_SECTION_TERMS',
+	array(
+		'Poetry',
+		'Fiction',
+		'Essays',
+	)
+);
+
+define(
+	'KKW_AUTHORS',
+	array(
+		'John Doe',
+	)
+);
+
+define(
+	'KKW_PUBLISHERS',
+	array(
+		'Best Books Publisher',
+	)
+);
+
+define(
+	'KKW_EXAMPLE_BOOKS',
+	array(
+		array(
+			'title'     => 'Nice poems',
+			'author'    => 'John Doe',
+			'section'   => 'Poetry',
+			'publisher' => 'Best Books Publisher',
+		),
+		array(
+			'title'     => 'A novel',
+			'author'    => 'John Doe',
+			'section'   => 'Fiction',
+			'publisher' => 'Best Books Publisher',
+		),
+		array(
+			'title'     => 'Short stories',
+			'author'    => 'John Doe',
+			'section'   => 'Fiction',
+			'publisher' => 'Best Books Publisher',
+		),
+		array(
+			'title'     => 'An essay',
+			'author'    => 'John Doe',
+			'section'   => 'Essays',
+			'publisher' => 'Best Books Publisher',
+		),
+	)
+);
+
+define(
+	'KKW_DEFAULT_TERMS',
+	array(
+		array(
+			'taxonomy' => KKW_BLOG_TYPE_TAXONOMY,
+			'items'    => KKW_BLOG_TYPE_TERMS,
+		),
+		array(
+			'taxonomy' => KKW_SECTION_TAXONOMY,
+			'items'    => KKW_SECTION_TERMS,
+		),
+		array(
+			'taxonomy' => KKW_AUTHOR_TAXONOMY,
+			'items'    => KKW_AUTHORS,
+		),
+		array(
+			'taxonomy' => KKW_PUBLISHER_TAXONOMY,
+			'items'    => KKW_PUBLISHERS,
+		),
+	)
+);
